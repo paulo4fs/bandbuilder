b46eee329a3a643366b9ec291d3d1041f605075b
paulosilva777 <engpaulofrota@gmail.com>
2020-08-11 19:30:00 -0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3333;
+
+app.get('/api/v1/users/', (req, res) => {
+  res.json('this is working');
+});
+
+app.post('/api/v1/signin', (req, res) => {
+  res.json('signed in');
+});
+
+app.listen(PORT, () => {
+  console.log(`app is running on port ${PORT}`);
+});
+
+/*
+api/v1/signin -> post - signin
+api/v1/signup -> post - signup
+api/v1/users/ -> get - get all users
+api/v1/users/:id -> show my profile
+api/v1/users/forgotpassword/:id -> patch - forgot password
+api/v1/users/resetpassword/:id -> delete - reset password
+api/v1/users/updatepassword/:id -> patch - update password
+api/v1/search?filters=#### -> get - find other users
+
+*/

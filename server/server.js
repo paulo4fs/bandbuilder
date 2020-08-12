e1464616e35120c9a6367f9d56f1d7aacc8dd5c6
paulosilva777 <engpaulofrota@gmail.com>
2020-08-12 20:15:14 -0300

    added pg

--- server/server.js
+++ server/server.js
@@ -19,11 +19,10 @@ app.listen(PORT, () => {
 /*
 api/v1/signin -> post - signin
 api/v1/signup -> post - signup
-api/v1/users/ -> get - get all users
-api/v1/users/:id -> show my profile
+api/v1/users -> get - get all users
+api/v1/users/:id -> get - show my profile
 api/v1/users/forgotpassword/:id -> patch - forgot password
 api/v1/users/resetpassword/:id -> delete - reset password
 api/v1/users/updatepassword/:id -> patch - update password
 api/v1/search?filters=#### -> get - find other users
-
 */

377f2267bf5502ba4b3b98314506732057de061c
paulo4fs <engpaulofrota@gmail.com>
2020-08-18 23:10:00 -0300

    building landing page

--- web/src/pages/Landing/index.js
+++ web/src/pages/Landing/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Button } from './style';
 
 const Landing = () => {
-  return <div>welcome to the Landing</div>;
+  return (
+    <div>
+      <h1>BandBuilder</h1>
+      <h3>Forme sua banda</h3>
+      <Button>Buscar</Button>
+      <button>Anunciar</button>
+    </div>
+  );
 };
 
 export default Landing;
---
+++ web/src/pages/Landing/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+    background: white;
+    border-radius: 2rem;
+    width: 150px;
+    height: 95px;
+`
